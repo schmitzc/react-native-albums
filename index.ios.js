1a1aca436e336d9d677b999a1042b4faac21c112
Cassie Koomjian <schmitzc@gmail.com>
2016-10-03 14:21:25 -0700

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+
+import Header from './src/components/header';
+
+const App = () => (
+  <Header />
+);
+
+AppRegistry.registerComponent('albums', () => App);
