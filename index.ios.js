c3b3327c6150606851b549a42ef89a3ee1b5eeab
Cassie Koomjian <schmitzc@gmail.com>
2016-10-03 17:34:22 -0700

    Add AlbumList component

--- index.ios.js
+++ index.ios.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { View, AppRegistry } from 'react-native';
 
-import Header from './src/components/header';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-  <Header headerText='Albums' />
+  <View>
+    <Header headerText='Albums' />
+    <AlbumList />
+  </View>
 );
 
 AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/AlbumList.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import { View, Text } from 'react-native';
+
+class AlbumList extends Component {
+  state = { albums: [] };
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
+
+  render() {
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
